d177673aebbcace37c0cb8402b1615069fd10add
Diego Macrini <dmacrini@gmail.com>
2023-08-09 11:39:12 +0200

    Update generateIndex.js

--- .github/scripts/generateIndex.js
+++ .github/scripts/generateIndex.js
@@ -4,7 +4,16 @@ const path = require('path');
 const generateIndex = (dirPath = '.') => {
     const files = fs.readdirSync(dirPath);
 
-    let htmlContent = '<html><body>';
+    let htmlContent = `<html>
+        <head><script type="module" src="https://cdn.skypack.dev/twind/shim"></script></head>
+        <body>
+            <main class="h-screen bg-purple-400 flex items-center justify-center">
+              <h1 class="font-bold text(center 5xl white sm:gray-800 md:pink-700)">Index</h1>
+            </main>
+            <a href="./tutorial"
+              class="px-6 py-3 text-blue-100 no-underline bg-blue-500 rounded hover:bg-blue-600 hover:underline hover:text-blue-200">
+              View tutorial
+            </a>`;
 
     files.forEach(file => {
         htmlContent += `<a href="${file}">${file}</a><br/>`;
